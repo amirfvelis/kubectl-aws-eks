57c0e5c4adc56e058a2f912db414fd1877339df8
amirf <65586306+amirfvelis@users.noreply.github.com>
2022-11-23 16:31:22 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,11 @@ else
     echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version)"
 fi
 
+echo "Downloading Argo"
 curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-darwin-amd64
+echo "Setting Chmod"
 chmod +x ./kubectl-argo-rollouts-darwin-amd64
+echo "Mv Argo Exec"
 mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
 
 sh -c "kubectl $*"
