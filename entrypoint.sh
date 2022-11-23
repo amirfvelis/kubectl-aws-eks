7d22b0d02b68c7f0e0df60b4c2aa34babccd1621
amirf <65586306+amirfvelis@users.noreply.github.com>
2022-11-23 16:05:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,6 @@ fi
 
 curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-darwin-amd64
 chmod +x ./kubectl-argo-rollouts-darwin-amd64
-sudo mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
+mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
 
 sh -c "kubectl $*"
